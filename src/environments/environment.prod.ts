aaf54e4180d38cf9094301561d87de1bb557a8e0
peterpeterparker <david.dalbusco@outlook.com>
2020-11-26 10:59:18 +0100

    build: capacitor, pwa, owlly-signature, firebase and prettier chore: copy assets and main files from original project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDbL9_UTZNXXOFpPQCJp2UaJko8rRLRAOk',
+    authDomain: 'project-owlly.firebaseapp.com',
+    databaseURL: 'https://project-owlly.firebaseio.com',
+    projectId: 'project-owlly',
+    storageBucket: 'project-owlly.appspot.com',
+    messagingSenderId: '336348094441',
+    appId: '1:336348094441:web:d28da171198dc32db93578',
+    measurementId: 'G-X7RVBQQPYP',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDbL9_UTZNXXOFpPQCJp2UaJko8rRLRAOk',
+    authDomain: 'project-owlly.firebaseapp.com',
+    databaseURL: 'https://project-owlly.firebaseio.com',
+    projectId: 'project-owlly',
+    storageBucket: 'project-owlly.appspot.com',
+    messagingSenderId: '336348094441',
+    appId: '1:336348094441:web:d28da171198dc32db93578',
+    measurementId: 'G-X7RVBQQPYP',
+  },
 };
 
 /*
