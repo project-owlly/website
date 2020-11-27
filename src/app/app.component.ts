5e22529cc28560f5f99211d3d9d4febc76519c13
peterpeterparker <david.dalbusco@outlook.com>
2020-11-27 15:58:20 +0100

    feat: info pwa reload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {SwUpdate} from '@angular/service-worker';
+
+import {first} from 'rxjs/operators';
+
+import {ToastService} from './services/toast.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  constructor(private swUpdate: SwUpdate, private toastService: ToastService) {}
+
+  async ngOnInit(): Promise<void> {
+    await this.checkSwUpdate();
+  }
+
+  private async checkSwUpdate(): Promise<void> {
+    if (!this.swUpdate.isEnabled) {
+      return;
+    }
+
+    this.swUpdate.available.pipe(first()).subscribe(async () => {
+      const {ToastComponent} = await import('./components/toast/toast.component');
+
+      await this.toastService.open(ToastComponent, {
+        msg: 'Eine neue Version ist verfügbar. Zum Aktualisieren neu laden.',
+        status: 'success',
+        position: 'top',
+      });
+    });
+
+    await this.swUpdate.checkForUpdate();
+  }
+}
