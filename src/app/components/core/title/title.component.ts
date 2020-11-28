62361e0d8eee692817efebbd631c17ce26de6c21
Sandro Scalco <sandro.scalco@liitu.ch>
2020-11-28 21:45:37 +0100

    fix constructor and show a second way to set the text.. probl. more readable

--- src/app/components/core/title/title.component.ts
+++ src/app/components/core/title/title.component.ts
@@ -3,13 +3,8 @@ import {AfterViewInit, Component, Input} from '@angular/core';
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
-  styleUrls: ['./title.component.scss']
+  styleUrls: ['./title.component.scss'],
 })
 export class TitleComponent {
-
- @Input() title: string;
-
- constructor() {
-   this.title = "";
- }
+  @Input() title: string = '';
 }
