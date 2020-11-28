56a37c6f5025b1ed7dd23aa78e64d99c28b8beaa
jonarivado <jonathan.dotzler@gmail.com>
2020-11-28 20:37:58 +0100

    first test with title component

--- src/app/components/core/components.module.ts
+++ src/app/components/core/components.module.ts
@@ -5,10 +5,11 @@ import {HeaderComponent} from './header/header.component';
 import {FooterComponent} from './footer/footer.component';
 import {ButtonComponent} from './button/button.component';
 import {SignatureComponent} from './signature/signature.component';
+import {TitleComponent} from './title/title.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, ButtonComponent, SignatureComponent],
-  exports: [HeaderComponent, FooterComponent, ButtonComponent, SignatureComponent],
+  declarations: [HeaderComponent, FooterComponent, ButtonComponent, SignatureComponent, TitleComponent],
+  exports: [HeaderComponent, FooterComponent, ButtonComponent, SignatureComponent, TitleComponent],
   imports: [CommonModule],
 })
 export class ComponentsModule {}
---
+++ src/app/components/core/title/title.component.ts
@@ -0,0 +1,15 @@
+import {AfterViewInit, Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-title',
+  templateUrl: './title.component.html',
+  styleUrls: ['./title.component.scss']
+})
+export class TitleComponent {
+
+ @Input() title: string;
+
+ constructor() {
+   this.title = "";
+ }
+}
