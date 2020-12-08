ebc4d6517fc4f933c986df7cf969a8f1e74cde41
jonarivado <jonathan.dotzler@gmail.com>
2020-12-08 20:51:27 +0100

    added all pages to /create
    can't submit it yet tho

---
+++ src/app/pages/create/createInterface.ts
@@ -0,0 +1,9 @@
+export interface createData {
+      text: string;
+      title: string;
+      type: string;
+      published: string;
+      author: string;
+      ruleValue: string;
+      goals: string[];
+    }
