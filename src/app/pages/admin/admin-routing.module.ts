28becea1be88cc998d45d7c2b788c759571684f3
Sandro Scalco <sandro.scalco@liitu.ch>
2020-12-11 15:55:08 +0100

    new routing

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -10,25 +10,13 @@ const routes: Routes = [
 
   {
     path: 'certify',
-    loadChildren: () => import('./certify/certify.module').then((m) => m.CertifyModule),
+    loadChildren: () => import('./certify-request/certify-request.module').then((m) => m.CertifyRequestModule),
     //canActivate:
   },
 
   {
     path: 'certify/:id',
     loadChildren: () => import('./certify-detail/certify-detail.module').then((m) => m.CertifyDetailModule),
-    //canActivate:
-  },
-
-  {
-    path: 'owlly',
-    loadChildren: () => import('./owlly/owlly.module').then((m) => m.OwllyModule),
-    //canActivate:
-  },
-
-  {
-    path: 'owlly/:id',
-    loadChildren: () => import('./owlly-detail/owlly-detail.module').then((m) => m.OwllyDetailModule),
     //canActivate:
   },
 ];
