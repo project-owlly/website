58c4eafe5ce13b3718460443b4dcfc22230a97be
Sandro Scalco <sandro.scalco@liitu.ch>
2020-12-11 16:11:44 +0100

    feat: new routing

--- src/app/pages/admin/admin-routing.module.ts
+++ src/app/pages/admin/admin-routing.module.ts
@@ -8,15 +8,49 @@ const routes: Routes = [
     //canActivate:
   },
 
+  // MUNICIPALITIES PAGES
   {
-    path: 'certify',
+    path: 'certify-request',
     loadChildren: () => import('./certify-request/certify-request.module').then((m) => m.CertifyRequestModule),
     //canActivate:
   },
-
   {
     path: 'certify/:id',
     loadChildren: () => import('./certify-detail/certify-detail.module').then((m) => m.CertifyDetailModule),
+    //canActivate:
+  },
+
+  // CAMPAINGER PAGES
+  {
+    path: 'campaign',
+    loadChildren: () => import('./campaigns/campaigns.module').then((m) => m.CampaignsModule),
+    //canActivate:
+  },
+  {
+    path: 'campaign/:id',
+    loadChildren: () => import('./campaign-details/campaign-details.module').then((m) => m.CampaignDetailsModule),
+    //canActivate:
+  },
+
+  // AUTH PAGES
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then((m) => m.LoginModule),
+    //canActivate:
+  },
+  {
+    path: 'logout',
+    loadChildren: () => import('./logout/logout.module').then((m) => m.LogoutModule),
+    //canActivate:
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then((m) => m.SignupModule),
+    //canActivate:
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./profile/profile.module').then((m) => m.ProfileModule),
     //canActivate:
   },
 ];
