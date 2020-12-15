68dafa17d16f09247d6690a5b1462c7c9bfa1fe4
jonarivado <jonathan.dotzler@gmail.com>
2020-12-15 22:01:33 +0100

    edit /explained page

--- src/app/pages/explained/explained.component.ts
+++ src/app/pages/explained/explained.component.ts
@@ -10,6 +10,20 @@ export class ExplainedComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function (this: any) {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      });
+    }
   }
 
 }
