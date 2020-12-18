9f6d49b97285a3b85f0cfe510c077cc81cb9560e
peterpeterparker <david.dalbusco@outlook.com>
2020-12-18 19:13:13 +0100

    fix: tailwind out of memory

--- src/app/pages/admin/login/login.component.ts
+++ src/app/pages/admin/login/login.component.ts
@@ -3,7 +3,6 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
   constructor() {}
--- src/app/pages/progress-cantons/progress-cantons.component.ts
+++ src/app/pages/progress-cantons/progress-cantons.component.ts
@@ -1,27 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router'
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-progress-cantons',
   templateUrl: './progress-cantons.component.html',
-  styleUrls: ['./progress-cantons.component.scss']
 })
 export class ProgressCantonsComponent implements OnInit {
-
   cantonSelect: string | null = '';
 
-  constructor(private _Activatedroute:ActivatedRoute) { }
+  constructor(private activatedroute: ActivatedRoute) {}
 
-  ngOnInit() {
-    this._Activatedroute.paramMap.subscribe(params => { 
-      this.cantonSelect = params.get('canton'); 
-  });
+  ngOnInit(): void {
+    this.activatedroute.paramMap.subscribe((params) => {
+      this.cantonSelect = params.get('canton');
+    });
   }
 
-  data = {sh: {text: "hallo", title: "gugus"},
-          zh: {text: "hallo", title: "gugus"}
-
-          }
-
-
+  data = {sh: {text: 'hallo', title: 'gugus'}, zh: {text: 'hallo', title: 'gugus'}};
 }
