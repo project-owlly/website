165db2b487bceecaaeaa29ee47fd4f7206823e4a
Sandro Scalco <sandro.scalco@liitu.ch>
2020-12-21 15:38:00 +0100

    feat: login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,59 @@
 import {Component, OnInit} from '@angular/core';
 
+import {AuthService} from 'src/app/services/auth.service';
+import {ToastService} from 'src/app/services/toast.service';
+
+import {FormGroup, Validators, FormBuilder} from '@angular/forms';
+import {Router} from '@angular/router';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
 })
 export class LoginComponent implements OnInit {
-  constructor() {}
+  public authForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private auth: AuthService, private router: Router, private toastService: ToastService) {
+    this.authForm = this.formBuilder.group({
+      username: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.minLength(6)],
+    });
+  }
 
   ngOnInit(): void {}
+
+  async login(authForm: FormGroup) {
+    if (!authForm.valid) {
+      const {ToastComponent} = await import('../../components/toast/toast.component');
+      await this.toastService.open(ToastComponent, {
+        msg: 'Form is not valid yet, current value: ' + authForm.value,
+        status: 'error',
+        position: 'bottom',
+      });
+      //console.log('Form is not valid yet, current value:', authForm.value);
+    } else {
+      console.log('Username: ' + authForm.value.username);
+
+      this.auth.login(authForm.value.username, authForm.value.password).then(
+        async (ok) => {
+          const {ToastComponent} = await import('../../components/toast/toast.component');
+
+          this.router.navigateByUrl('/admin');
+
+          await this.toastService.open(ToastComponent, {
+            msg: 'ok',
+            status: 'success',
+            position: 'bottom',
+          });
+        },
+        async (err) => {
+          const {ToastComponent} = await import('../../components/toast/toast.component');
+          await this.toastService.open(ToastComponent, {
+            msg: JSON.parse(err.message).error.message,
+            status: 'error',
+            position: 'bottom',
+          });
+        }
+      );
+    }
+  }
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,13 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LoginComponent} from './login.component';
 
 import {LoginRoutingModule} from './login-routing.module';
 import {ComponentsModule} from 'src/app/components/core/components.module';
 
 @NgModule({
-  imports: [CommonModule, LoginRoutingModule, ComponentsModule],
+  imports: [CommonModule, LoginRoutingModule, ComponentsModule, FormsModule, ReactiveFormsModule],
   declarations: [LoginComponent],
 })
 export class LoginModule {}
