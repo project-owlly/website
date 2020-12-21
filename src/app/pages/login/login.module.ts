14a7b5438e2a72687fdd5bc6f5515c50d7daeacc
Sandro Scalco <sandro.scalco@liitu.ch>
2020-12-21 14:55:49 +0100

    feat: page not foudn fix

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,13 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
-import {LoginRoutingModule} from './login-routing.module';
 import {LoginComponent} from './login.component';
 
+import {LoginRoutingModule} from './login-routing.module';
 import {ComponentsModule} from 'src/app/components/core/components.module';
 
 @NgModule({
-  declarations: [LoginComponent],
   imports: [CommonModule, LoginRoutingModule, ComponentsModule],
+  declarations: [LoginComponent],
 })
 export class LoginModule {}
