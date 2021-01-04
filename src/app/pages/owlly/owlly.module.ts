ca1ab66293da2e917096fb997702f5fa83903c74
jonarivado <jonathan.dotzler@gmail.com>
2021-01-04 19:11:24 +0100

    changed /o design
    resolves #71

--- src/app/pages/owlly/owlly.module.ts
+++ src/app/pages/owlly/owlly.module.ts
@@ -3,9 +3,9 @@ import {NgModule} from '@angular/core';
 import {ScullyLibModule} from '@scullyio/ng-lib';
 import {OwllyRoutingModule} from './owlly-routing.module';
 import {OwllyComponent} from './owlly.component';
-
+import {ComponentsModule} from 'src/app/components/core/components.module';
 @NgModule({
   declarations: [OwllyComponent],
-  imports: [CommonModule, OwllyRoutingModule, ScullyLibModule],
+  imports: [CommonModule, OwllyRoutingModule, ScullyLibModule, ComponentsModule],
 })
 export class OwllyModule {}
