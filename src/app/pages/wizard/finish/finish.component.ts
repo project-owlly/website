015a0153d7a1be04a45734a18f6e871a6ac75910
Sandro Scalco <sandro.scalco@liitu.ch>
2021-01-05 19:08:04 +0100

    new social Share feature

--- src/app/pages/wizard/finish/finish.component.ts
+++ src/app/pages/wizard/finish/finish.component.ts
@@ -7,6 +7,12 @@ import {first} from 'rxjs/operators';
 import {Owlly} from '../../../types/owlly';
 
 import {OwllyRoutingService} from '../../../services/owlly-routing.service';
+import {faTwitter, faFacebook, faWhatsapp, faLinkedin} from '@fortawesome/free-brands-svg-icons';
+
+import {faEnvelope, faCopy} from '@fortawesome/free-solid-svg-icons';
+
+import {Plugins} from '@capacitor/core';
+const {Share, Device} = Plugins;
 
 @Component({
   selector: 'app-finish',
@@ -14,7 +20,81 @@ import {OwllyRoutingService} from '../../../services/owlly-routing.service';
   styleUrls: ['./finish.component.scss'],
 })
 export class FinishComponent {
+  // Social Share Web Component
+  shareSocialShareOptions: any;
+  showSocialShare = false;
+  faTwitter = faTwitter;
+  faFacebook = faFacebook;
+  faWhatsapp = faWhatsapp;
+  faLinkedin = faLinkedin;
+  faEnvelope = faEnvelope;
+  faCopy = faCopy;
+
   readonly owlly$: Observable<Owlly | undefined> = this.owllyRoutingService.owlly(this.activatedRoute.paramMap).pipe(first());
 
   constructor(private activatedRoute: ActivatedRoute, private owllyRoutingService: OwllyRoutingService) {}
+
+  async share() {
+    const device = await Device.getInfo();
+
+    this.owlly$.subscribe(async (owlly: any) => {
+      if (device.platform === 'web' && navigator && navigator['share']) {
+        let shareRet = await Share.share({
+          title: owlly.data.title,
+          text: String(owlly.data.text).substr(0, 50),
+          url: 'https://owlly.ch/o/' + owlly.data.slug,
+          dialogTitle: owlly.data.title,
+        }).catch((onrejected) => {});
+      } else {
+        await this.shareFallback(owlly);
+      }
+    });
+  }
+
+  shareFallback(owlly: any) {
+    return new Promise(async (resolve) => {
+      const shareUrl = 'https://owlly.ch/o/' + owlly.data.slug;
+
+      // The configuration, set the share options
+      this.shareSocialShareOptions = {
+        displayNames: true,
+        config: [
+          {
+            twitter: {
+              socialShareUrl: shareUrl,
+              socialSharePopupWidth: 300,
+              socialSharePopupHeight: 400,
+            },
+          },
+          {
+            facebook: {
+              socialShareUrl: shareUrl,
+            },
+          },
+          {
+            whatsapp: {
+              socialShareUrl: shareUrl,
+            },
+          },
+          {
+            linkedin: {
+              socialShareUrl: shareUrl,
+            },
+          },
+          {
+            email: {
+              socialShareBody: shareUrl,
+            },
+          },
+          {
+            copy: {
+              socialShareUrl: shareUrl,
+            },
+          },
+        ],
+      };
+      this.showSocialShare = true;
+      resolve(true);
+    });
+  }
 }
