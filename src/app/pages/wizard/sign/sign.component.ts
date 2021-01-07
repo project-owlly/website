49365715f12b88fc0eb2dd8831c6da145d394fce
Sandro Scalco <sandro.scalco@liitu.ch>
2021-01-07 17:49:08 +0100

    params instead of query params

--- src/app/pages/wizard/sign/sign.component.ts
+++ src/app/pages/wizard/sign/sign.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ActivatedRoute, Params, Router, ParamMap} from '@angular/router';
 import {Observable} from 'rxjs';
 
 import {Pdf} from '../../../types/pdf';
@@ -18,7 +18,7 @@ const {Browser, Device} = Plugins;
 export class SignComponent {
   public deviceInfo: DeviceInfo | undefined;
 
-  readonly owllyId$: Observable<string | undefined> = this.route.queryParams.pipe(
+  readonly owllyId$: Observable<string | undefined> = this.route.params.pipe(
     first(),
     filter((params: Params) => params.owllyId !== null),
     map((params: Params) => params.owllyId),
@@ -53,13 +53,30 @@ export class SignComponent {
       });
   }
   navigate(): void {
+    this.owllyId$
+      .pipe(
+        filter((owllyId: string | undefined) => owllyId !== undefined),
+        first()
+      )
+      .subscribe(async (owllyId: string | undefined) => {
+        await this.router.navigate(['/finish', owllyId]).catch((err) => {
+          console.log(err.message);
+        });
+      });
+
+    /*    
+    this.route.params.pipe(first()).subscribe( async (owllyId)=>{
+      await this.router.navigate(['/finish', owllyId]).catch((err) => {
+        console.log(err.message);
+      });
+    })
     this.route.queryParams.pipe(first()).subscribe(async (owllyId) => {
       await this.router.navigate(['/finish', owllyId]).catch((err) => {
         console.log(err.message);
       });
     });
-
-    /*    this.owllyId$
+    
+    this.owllyId$
       .pipe(
         filter((owllyId: string | undefined) => owllyId !== undefined),
         first()
