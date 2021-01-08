2db6a064948901c6e85c0d40fb7ef32ab62d525d
jonarivado <jonathan.dotzler@gmail.com>
2021-01-08 21:28:50 +0100

    added progress-bund, added links, deactivated incomplete stuff

---
+++ src/app/pages/progress-bund/progress-bund.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bund',
+  templateUrl: './progress-bund.component.html',
+  styleUrls: ['./progress-bund.component.scss']
+})
+export class ProgressBundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
