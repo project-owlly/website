060fcdeb559d8a1f0dec4765d2af84241a045185
jonarivado <jonathan.dotzler@gmail.com>
2021-01-08 15:27:24 +0100

    content for campaigner & citizen

--- src/app/pages/campaigner/campaigner.component.ts
+++ src/app/pages/campaigner/campaigner.component.ts
@@ -3,6 +3,7 @@ import {Component, OnInit} from '@angular/core';
 @Component({
   selector: 'app-campaigner',
   templateUrl: './campaigner.component.html',
+  styleUrls: ['./campaigner.component.scss'],
 })
 export class CampaignerComponent implements OnInit {
   constructor() {}
