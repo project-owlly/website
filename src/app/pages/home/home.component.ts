446d0aa4100da2f9063a7ca7654415d14e09bf56
jonarivado <jonathan.dotzler@gmail.com>
2021-01-10 17:50:41 +0100

    added feedback button
    resolves #148

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {ScullyRoutesService, ScullyRoute} from '@scullyio/ng-lib';
 import {Observable} from 'rxjs';
 import {map} from 'rxjs/operators';
-import {faAngleRight, faAngleLeft} from '@fortawesome/free-solid-svg-icons';
+import {faAngleRight, faAngleLeft, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {ModalService} from '../../services/modal.service';
+import {FeedbackComponent as FeedbackComponentType} from '../../modals/feedback/feedback.component';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +14,18 @@ import {faAngleRight, faAngleLeft} from '@fortawesome/free-solid-svg-icons';
 export class HomeComponent implements OnInit {
   faAngleLeft = faAngleLeft;
   faAngleRight = faAngleRight;
+  faEnvelope = faEnvelope;
   owlly$: Observable<ScullyRoute[]> | undefined;
 
-  constructor(private scully: ScullyRoutesService) {}
+  constructor(private scully: ScullyRoutesService, private modalService: ModalService<FeedbackComponentType>) {}
 
   ngOnInit(): void {
     this.owlly$ = this.scully.available$.pipe(map((routes: any) => routes.filter((route: any) => route.route.startsWith('/o/'))));
+  }
+
+  async showFeedback(): Promise<void> {
+    const {FeedbackComponent} = await import('../../modals/feedback/feedback.component');
+
+    await this.modalService.open(FeedbackComponent);
   }
 }
