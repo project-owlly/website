e5608f5e8718d17ba32426c4f6deeddcb09d37b6
Sandro Scalco <sandro.scalco@liitu.ch>
2021-01-11 14:06:41 +0100

    Fix support/*

--- src/app/pages/citizen/citizen.component.ts
+++ src/app/pages/citizen/citizen.component.ts
@@ -2,13 +2,14 @@ import {Component, OnInit} from '@angular/core';
 import {ModalService} from '../../services/modal.service';
 import {QuestionComponent as QuestionComponentType} from '../../modals/question/question.component';
 import {NewsletterComponent as NewsletterComponentType} from '../../modals/newsletter/newsletter.component';
+import {InquiryComponent as InquiryComponentType} from '../../modals/inquiry/inquiry.component';
 @Component({
   selector: 'app-citizen',
   templateUrl: './citizen.component.html',
   styleUrls: ['./citizen.component.scss'],
 })
 export class CitizenComponent implements OnInit {
-  constructor(private modalService: ModalService<NewsletterComponentType | QuestionComponentType>) {}
+  constructor(private modalService: ModalService<NewsletterComponentType | QuestionComponentType | InquiryComponentType>) {}
 
   ngOnInit(): void {}
 
@@ -16,5 +17,13 @@ export class CitizenComponent implements OnInit {
     const {NewsletterComponent} = await import('../../modals/newsletter/newsletter.component');
 
     await this.modalService.open(NewsletterComponent);
+  }
+  async showInquiry(): Promise<void> {
+    /*const {FeedbackComponent} = await import('../../modals/feedback/feedback.component');
+
+    await this.modalService.open(FeedbackComponent);
+    */
+    const {InquiryComponent} = await import('../../modals/inquiry/inquiry.component');
+    await this.modalService.open(InquiryComponent);
   }
 }
