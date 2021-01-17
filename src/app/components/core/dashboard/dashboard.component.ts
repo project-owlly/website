9b1fc8e8ef69e8377ea305609468bf0737745b0d
jonarivado <jonathan.dotzler@gmail.com>
2021-01-17 22:40:29 +0100

    added chart.js

--- src/app/components/core/dashboard/dashboard.component.ts
+++ src/app/components/core/dashboard/dashboard.component.ts
@@ -11,9 +11,36 @@ export class DashboardComponent implements OnInit {
 
   faUserCircle=faUserCircle;
 
+
   constructor() { }
 
   ngOnInit(): void {
+    const Chart = require('chart.js');
+    var ctx = document.getElementById('signatures')!;
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data:{
+        labels: [1,2,3,4,5,6],
+        datasets: [{
+          data: [10,20,14,57,76,87],
+          label: 'Renteninitiative',
+          borderColor: '#00a6d4',
+          backgroundColor: '#00a6d4',
+          fill: false,
+        },
+        {
+          data: [5,13,69,65,30,44],
+          label: 'Velo-Initiative',
+          borderColor: '#81bc4f',
+          backgroundColor: '#81bc4f',
+          fill: false,
+        },
+      ]
+      },
+      options: {
+        tooltips: {enabled: false}
+      }
+  });
   }
 
   openMobileNav() {
