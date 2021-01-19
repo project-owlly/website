5b1b7e75755336dcc7490d40c7ba629b8eba2988
Sandro Scalco <sandro.scalco@liitu.ch>
2021-01-19 19:13:42 +0100

    feat

---
+++ src/app/pages/solutions/administration/administration.component.ts
@@ -0,0 +1,23 @@
+import {Component, OnInit} from '@angular/core';
+import {ModalService} from '../../../services/modal.service';
+import {InquiryComponent as InquiryComponentType} from '../../../modals/inquiry/inquiry.component';
+
+@Component({
+  selector: 'app-administration',
+  templateUrl: './administration.component.html',
+  styleUrls: ['./administration.component.scss'],
+})
+export class AdministrationComponent implements OnInit {
+  constructor(private modalService: ModalService<InquiryComponentType>) {}
+
+  ngOnInit(): void {}
+
+  async showInquiry(): Promise<void> {
+    /*const {FeedbackComponent} = await import('../../modals/feedback/feedback.component');
+
+    await this.modalService.open(FeedbackComponent);
+    */
+    const {InquiryComponent} = await import('../../../modals/inquiry/inquiry.component');
+    await this.modalService.open(InquiryComponent);
+  }
+}
