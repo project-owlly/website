07a75e7b65d51fa6a41666ff24a29e246b14a3a7
Sandro Scalco <sandro.scalco@liitu.ch>
2021-02-28 13:45:14 +0100

    owlly services

--- src/app/services/owlly-campaigner.service.ts
+++ src/app/services/owlly-campaigner.service.ts
@@ -1,8 +1,14 @@
 import {Injectable} from '@angular/core';
+import {AngularFirestore} from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OwllyCampaignerService {
-  constructor() {}
+  constructor(private firestore: AngularFirestore) {}
+
+  getOwlly() {
+    //this.firestore.collectionGroup('8200').get().  ('owlly'). .ref.where("status", "==", true).get().then((querySnapshot=>{
+    //}))
+  }
 }
