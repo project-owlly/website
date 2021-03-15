acc3c82f62e55741e87e993970b58d910891646a
Sandro Scalco <sandro.scalco@liitu.ch>
2021-03-15 16:52:46 +0100

    feat: Skribble

--- src/app/types/pdf.ts
+++ src/app/types/pdf.ts
@@ -1,3 +1,14 @@
 export interface Pdf {
   url: string | undefined;
+  message: string;
+  status: {
+    owlly: boolean;
+    ots: boolean;
+    skribble: boolean;
+  };
+  skribble: {
+    signatureRequest: string;
+    documentId: string;
+    skribbleSigningUrl: string;
+  };
 }
