1228a493b9bdd4ea5703b78610dd945bc6a77177
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-19 14:45:35 +0200

    feat modules

--- src/app/pages/verify/verify.module.ts
+++ src/app/pages/verify/verify.module.ts
@@ -3,9 +3,11 @@ import {CommonModule} from '@angular/common';
 
 import {VerifyRoutingModule} from './verify-routing.module';
 import {VerifyComponent} from './verify.component';
+import {ComponentsModule} from 'src/app/components/core/components.module';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [VerifyComponent],
-  imports: [CommonModule, VerifyRoutingModule],
+  imports: [CommonModule, VerifyRoutingModule, ComponentsModule, FontAwesomeModule],
 })
 export class VerifyModule {}
