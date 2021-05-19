daaaaf2a87030ed457de25e47194958941ebf80b
Sandro Scalco <sandro.scalco@liitu.ch>
2021-05-19 23:07:43 +0200

    fat update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,22 @@
-module.exports = (isProd) => ({
-  prefix: '',
-  purge: {
-    enabled: isProd,
-    content: ['./src/**/*.{html,ts}'],
-  },
+module.exports = {
+  // mode: 'jit',
+  purge: ['./src/**/*.{html,ts}'],
   darkMode: 'media', // or 'media' or 'class'
   theme: {
     extend: {
-      height: {
-        pagehero: '500px',
-      },
       colors: {
-        primary: 'var(--color-primary)',
-        secondary: 'var(--color-secondary)',
-        'secondary-dark': 'var(--color-secondary-dark)',
-        tertiary: 'var(--color-tertiary)',
-        'tertiary-dark': 'var(--color-tertiary-dark)',
+        primary: {
+          default: 'var(--color-primary)',
+          dark: 'var(--color-primary-dark)',
+        },
+        secondary: {
+          default: 'var(--color-secondary)',
+          dark: 'var(--color-secondary-dark)',
+        },
+        tertiary: {
+          default: 'var(--color-tertiary)',
+          dark: 'var(--color-tertiary-dark)',
+        },
         success: 'var(--color-success)',
         warning: 'var(--color-warning)',
         danger: 'var(--color-danger)',
@@ -23,24 +24,10 @@ module.exports = (isProd) => ({
         medium: 'var(--color-medium)',
         light: 'var(--color-light)',
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            a: {
-              color: 'var(--color-tertiary)',
-              'text-decoration': 'none',
-              '&:hover': {
-                color: 'var(--color-tertiary)',
-                'text-decoration': 'underline',
-              },
-            },
-          },
-        },
-      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [require('@tailwindcss/typography')],
-});
+};
